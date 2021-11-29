3d901f729e46dbb908a16ce59f177ee57cc72960
tolu-ayobami <70198065+tolu-ayobami@users.noreply.github.com>
2021-11-29 16:29:15 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-var btn = document.querySelector("#menu-btn");
-var side = document.querySelector(".side-bar");
-btn.addEventListener("click", function(){
-    side.classList.toggle("menu-btn");
-
-});
