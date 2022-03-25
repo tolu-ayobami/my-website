e656d134861f0e648bb39d30df341955ff24aa11
tolu-ayobami <70198065+tolu-ayobami@users.noreply.github.com>
2022-03-25 02:21:07 +0100

    Add files via upload

---
+++ dashboard project/app.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector("#menu-btn");
+var side = document.querySelector(".side-bar");
+btn.addEventListener("click", function(){
+    side.classList.toggle("menu-btn");
+
+});
