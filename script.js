832f695ca6bd691d9863afd72fec68dc9126f9e3
tolu-ayobami <70198065+tolu-ayobami@users.noreply.github.com>
2022-04-21 22:51:44 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var links = document.querySelector(".link");
-var bar = document.querySelector(".bars9");
-
-bar.addEventListener("click", function(){
-    bar.classList.toggle("active");
-    links.classList.toggle("active");
-
-});
