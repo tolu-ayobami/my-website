3a60dafa028dceafdbd90d57a29345f057946446
Tolu <70198065+tolu-ayobami@users.noreply.github.com>
2022-10-15 18:12:59 +0100

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -3,13 +3,20 @@ var teg1 = document.querySelector("#ic");
 var teg2 = document.querySelector(".inp");
 var feg = document.querySelector("#close");
 var yup = document.querySelector(".record");
+var send = document.querySelector(".check");
+var tolu = document.querySelector(".tolu");
+
+Send.onclick = function (){
+Tolu.style.display ="block";
 
 
+}
 
 teg.onclick = function (){
    teg.style.display ="none";
    teg1.style.display ="block";
    yup.style.display ="block";
+   tolu.style.display ="none";
 }
 
 
